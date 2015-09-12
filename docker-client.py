133baeeb988dcac3c5b4100c32f644caa0ff8dd5
Alex Collins <alcollin@redhat.com>
2015-09-12 13:23:01 -0400

    started inspect

--- docker-client.py
+++ docker-client.py
@@ -1,7 +1,7 @@
 
 import util
 import json
-
+from docker import Client
 
 """ makeshift docker api suited for RHAI """
 
@@ -24,7 +24,7 @@ class DockerClient:
         return return_dict
 
     def images(self):
-        """ returns a list of all iamge ids """
+        """ returns a list of all image ids """
 
         cmd = ['docker', 'images', '-q', '--no-trunc']
         out = util.subp(cmd)
@@ -33,19 +33,63 @@ class DockerClient:
         for line in trim_out.split('\n'):
             return_list.append(line)
 
+        # return a set to remove dup ID's
+        return list(set(return_list))
+
+    def containers(self, running=True):
+        """ returns a list of all container ids """
+
+        if running:
+            cmd = ['docker', 'ps', '-q', '--no-trunc']
+        else:
+            cmd = ['docker', 'ps', '-a', '-q', '--no-trunc']
+        out = util.subp(cmd)
+        trim_out = out.stdout.strip()
+        return_list = []
+        for line in trim_out.split('\n'):
+            return_list.append(line)
+
         return return_list
+
+    def inspect(self, obj_id):
+        """ returns a dict of the inspection of a container or image """
+
+        if self._is_a_container(obj_id):
+            return self._container_inspect(obj_id)
+
+        return self._image_inspect(obj_id)
+
+    def _container_inspect(self, obj_id):
+        pass
+        #FIXME the return dicts will have multiple keys, need to make a 
+        # to dict converter in util
+
+    def _image_inspect(self, obj_id):
+        pass
+
+    def _is_a_container(self, obj_id):
+        """ returns true if obj ID is a container ID"""
+
+        conts = self.containers(False)
+        if obj_id in conts:
+            return True
+
+        return False
 
     def dprint(self, json_obj):
         """debug method for printing"""
 
-        print json.dumps(json_obj,indent=2)
+        print json.dumps(json_obj, indent=2)
 
 if __name__ == '__main__':
 
     x = DockerClient()
-    tmp = x.images()
 
-    for line in tmp:
-        print line
+    x.inspect("ea061f56e49899b605cb1157731a53f50fe97c35d714f2835e540253928dc9e0 ")
 
-    x.dprint(x.info())
+
+    #c = Client(base_url='unix://var/run/docker.sock')
+    # print json.dumps(c.info(),indent=2)
+
+
+
